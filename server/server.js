eac59592e269ff2f762368de15ade2d03ca775b9
Michelle Soliman <91602286+soliman-michelle@users.noreply.github.com>
2023-12-17 22:00:21 +0800

    db

--- server/server.js
+++ server/server.js
@@ -21,10 +21,10 @@
     app.use('/uploads', express.static('uploads'));
 
     const db = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "scheduling"
+      host: process.env.DB_HOST,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DBNAME
     });
 
     const storage = multer.diskStorage({
