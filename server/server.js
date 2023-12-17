8b9dce8b0b06aed4bd5e1b1f3a398d0adefa2571
Michelle Soliman <91602286+soliman-michelle@users.noreply.github.com>
2023-12-17 23:31:30 +0800

    link

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@
     const app = express();
     app.use(express.json());
     app.use(cors({
-      origin: ["http://localhost:3000", "https://ccsched-michelle-solimans-projects.vercel.app"],
+      origin: ["http://localhost:3000", "http://ccsched-michelle-solimans-projects.vercel.app"],
       methods: ["POST", "GET"],
       credentials: true
     }));
