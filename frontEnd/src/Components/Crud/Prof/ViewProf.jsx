6995843a244b8f7510a896bac67facc152d0a618
Michelle Soliman <91602286+soliman-michelle@users.noreply.github.com>
2024-02-13 07:18:19 +0800

    balik

--- frontEnd/src/Components/Crud/Prof/ViewProf.jsx
+++ frontEnd/src/Components/Crud/Prof/ViewProf.jsx
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import ReactPaginate from 'react-paginate';
 import AddProf from './AddProf';
 import { faPen } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -13,11 +12,6 @@ const ViewProf = () => {
   const [showEditUserModal, setShowEditUserModal] = useState(false);
   const [selectedUser, setSelectedUser] = useState(null);
   const [showModalDelete, setShowModalDelete] = useState(false);
-  const [currentPage, setCurrentPage] = useState(0);
-  const [itemsPerPage, setItemsPerPage] = useState(10);
-  const indexOfLastItem = (currentPage + 1) * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = user.slice(indexOfFirstItem, indexOfLastItem);
 
   const toggleAddUserModal = () => {
     setShowAddUserModal(!showAddUserModal);
@@ -121,137 +115,33 @@ const ViewProf = () => {
   
   
 
-  const handlePageChange = (selected) => {
-    setCurrentPage(selected.selected);
-    };
-  
-    return (
-      <div>
-        <style>
-          {`
-  
-  @media (min-width: 768px) {
-    .container{
-      width:100%;
-    }
-  }
-  
-  .container header .filterEntries {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-  
-  .filterEntries .entries {
-    color: #000000;
-  }
-  
-  .filterEntries.entries select, .filterEntries .filter input {
-    padding: 7px 10px;
-    border: 1px solid #000000;
-    color: #ffffff;
-    background: #ffffff;
-    border-radius: 5px;
-    outline: none;
-    transition: 0.3s;
-    cursor: pointer;
-  }
-  
-  .filterEntries .entries select{
-    padding: 5px 10px;
-  }
-  
-  .filterEntries .filter {
-    display: flex;
-    align-items: center;
-  }
-  
-  .filter label {
-    color:#ffffff;
-    margin-right: 5px;
-  }
-  
-  .filter input:focus{
-    border-color: rgb(255, 0, 162);
-  }
-          .custom-table th,
-          .custom-table td{
-            border: 4px
-            solid #ffff;
-          }
-  
-          .content .card{
-            padding: 20px;
-            padding-top: 30px;
-            margin-left: 10px;
-            margin-right:10px;
-            margin-top: 20px;
-            font-size: 14px;
-            border-radius: 10px;
-            background-image: url('/cover.png');
-            background-size: 100%;
-            background-position: center;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-            color: #ffffff;
-  }
-  
-  @media (max-width: 768px) {
-    .custom-table {
-      overflow-x: auto;
-      white-space: nowrap; /* Prevent line breaks */
-    }
-  }
-  
-          
-          `}
-  
-        </style>
-        <div className = "content">
-      <div className = "card">
-        <h1>PROFESSORS</h1>
-      </div>
-      </div>
-      <section className="home-section pt-2">
-      <div className='container-fluid'>
+  return (
+    <div>
+      <section className="home-section">
+        <div className='container'>
           <div className='row'>
-
-          <header>
-            <div className='filterEntries'>
+            <div className='col-md-12'>
               <AddProf
                 show={showAddUserModal}
                 handleClose={toggleAddUserModal}
                 handleAdd={handleAddUser}
               />
-              <div className="entries">
-              Show {' '}    
-              <select name="" id="table_size" onChange={(e) => setItemsPerPage(parseInt(e.target.value, 10))}>
-              <option value="10">10</option>
-              <option value="20">20</option>
-              <option value="50">50</option>
-              <option value="100">100</option>
-              </select> entries
-            </div>
-              </div>
-              </header>
 
-              <div className="card-body">
-              <div className='col-md-12 col-md-12 ps-2 pe-2 overflow-auto'>
-              <table className='custom-table table table-striped table-hover .table-responsive mt-2'>
+              <table className='table table-bordered'>
                 <thead>
-                <tr className = "table-primary border-dark">
+                  <tr>
                     <th>No.</th>
                     <th>Name</th>
                     <th>Role</th>
                     <th>Actions</th>
                   </tr>
                 </thead>
-
                 <tbody>
-                {currentItems.map((userItem, id) => {
+                {user.map((userItem, id) => {
                   return (
                     <tr key={userItem.User_id}>
-                      <td>{id + 1 + currentPage * itemsPerPage}</td>
-                      <td>{userItem.fname + " " + userItem.mname + " " + userItem.lname}</td>
+                      <td>{id + 1}</td>
+                      <td>{userItem.fname + " " + userItem.lname}</td>
                       <td>{userItem.roleName}</td>
                       <td>
                         <button
@@ -274,25 +164,7 @@ const ViewProf = () => {
               </table>
             </div>
           </div>
-          <footer>
-              <span className="showEntries">
-  Showing {indexOfFirstItem + 1} - {Math.min(indexOfLastItem, user.length)} of {user.length} entries
-</span>
-              <ReactPaginate
-                previousLabel={'< previous'}
-                nextLabel={'next >'}
-                breakLabel={'...'}
-                pageCount={Math.ceil(user.length / itemsPerPage)}
-                marginPagesDisplayed={2}
-                pageRangeDisplayed={5}
-                onPageChange={handlePageChange}
-                containerClassName={'pagination-container'}
-                subContainerClassName={'pages pagination'}
-                activeClassName={'active'}
-      />
-      </footer>
-          </div>
-          </div>
+        </div>
       </section>
       {showEditUserModal && (
         <EditProf
