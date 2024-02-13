ed0dedfc352a753a871dbb94c944ffc516bddc00
Michelle Soliman <91602286+soliman-michelle@users.noreply.github.com>
2024-02-13 08:17:36 +0800

    sa

--- frontEnd/src/Components/ViewAccount.jsx
+++ frontEnd/src/Components/ViewAccount.jsx
@@ -2,20 +2,13 @@ import { useState, useEffect } from "react"
 import DeleteAccount from "./DeleteAccount";
 import axios from "axios";
 import AddAccount from "./AddAccount";
-import Header from './Header';
-import Sidebar from './Sidebar';
 
 const ViewAccount = () => {
     const [user, setUser] = useState([]);
     const [showAddUserModal, setShowAddUserModal] = useState(false);
     const [showModalDelete, setShowModalDelete] = useState(false);
     const [showDeleteSuccessAlert, setShowDeleteSuccessAlert] = useState(false);
-    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
-    const toggleSidebar = () => {
-      setIsSidebarOpen(!isSidebarOpen);
-    };
-    
     const toggleAddUserModal = () => {
         setShowAddUserModal(!showAddUserModal);
       };
@@ -32,7 +25,7 @@ const ViewAccount = () => {
   
       const fetchData = async () => {
         try {
-          const res = await axios.get("https://ccsched.onrender.com/user");
+          const res = await axios.get("http://localhost:8081/user");
           setUser(res.data);
         } catch (err) {
           console.error(err);
@@ -46,9 +39,9 @@ const ViewAccount = () => {
       const handleDeleteUser = async (userId) => {
         try {
           console.log(`Deleting user with ID: ${userId}`);
-          await axios.delete(`https://ccsched.onrender.com/user/` + userId + `/delete`);
+          await axios.delete(`http://localhost:8081/user/` + userId + `/delete`);
           console.log(`User with ID ${userId} deleted successfully`);
-          setUser((prevUser) => prevUser.filter((userItem) => userItem.user_id !== userId)); 
+          setUser((prevUser) => prevUser.filter((userItem) => userItem.user_id !== userId)); // Update the filter condition
           setShowDeleteSuccessAlert(true);
         setTimeout(() => {
           setShowDeleteSuccessAlert(false);
@@ -60,99 +53,7 @@ const ViewAccount = () => {
   
   return (
     <div>
-            <style>
-        {`
-
-@media (min-width: 768px) {
-  .container{
-    width:100%;
-  }
-}
-
-.container header .filterEntries {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.filterEntries .entries {
-  color: #000000;
-}
-
-.filterEntries.entries select, .filterEntries .filter input {
-  padding: 7px 10px;
-  border: 1px solid #000000;
-  color: #ffffff;
-  background: #ffffff;
-  border-radius: 5px;
-  outline: none;
-  transition: 0.3s;
-  cursor: pointer;
-}
-
-.filterEntries .entries select{
-  padding: 5px 10px;
-}
-
-.filterEntries .filter {
-  display: flex;
-  align-items: center;
-}
-
-.filter label {
-  color:#ffffff;
-  margin-right: 5px;
-}
-
-.filter input:focus{
-  border-color: rgb(255, 0, 162);
-}
-        .custom-table th,
-        .custom-table td{
-          border: 4px
-          solid #ffff;
-        }
-
-        @media (max-width: 768px) {
-          .custom-table {
-            overflow-x: auto;
-            white-space: nowrap; /* Prevent line breaks */
-          }
-        }
-        
-
-        .content .card{
-          padding: 20px;
-          padding-top: 30px;
-          margin-left: 10px;
-          margin-right:10px;
-          margin-top: 20px;
-          font-size: 14px;
-          border-radius: 10px;
-          background-image: url('/cover.png');
-          background-size: 100%;
-          background-position: center;
-          box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-          color: #ffffff;
-}
-        
-        `}
-
-      </style>
-      <div className="h-100">
-    <div className="wrapper">
-    <Sidebar isSidebarOpen={isSidebarOpen} />
-      <div className = "main">
-        <Header toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen}/>
-        <div className = "container">
-
-        <div className = "content">
-      <div className = "card">
-      <h1 style="color: black;">ACCOUNTS</h1>
-      </div>
-      </div>
-
-      <section className="home-section pt-2">
+    
         {/* Alert for successful deletion */}
      {showDeleteSuccessAlert && (
         <div className="alert alert-danger alert-dismissible fade show" role="alert">
@@ -160,34 +61,19 @@ const ViewAccount = () => {
           <button type="button" className="btn-close" onClick={() => setShowDeleteSuccessAlert(false)} aria-label="Close"></button>
         </div>
       )}
-
+        <section className="home-section">
           <div className='container'>
             <div className='row'>
-
-            <header>
-            <div className='filterEntries'>
+              <div className='col-md-12'>
                 <AddAccount
                   show={showAddUserModal}
                   handleClose={toggleAddUserModal}
                   handleAdd={handleAddUser}
                 />
-              <div className="entries">
-              Show {' '}    
-              <select name="" id="table_size" onChange={(e) => setItemsPerPage(parseInt(e.target.value, 10))}>
-              <option value="10">10</option>
-              <option value="20">20</option>
-              <option value="50">50</option>
-              <option value="100">100</option>
-              </select> entries
-            </div>
-            </div>
-            </header>
 
-            <div className="card-body">
-            <div className='col-md-12 ps-2 pe-2 overflow-auto'>
-              <table className='custom-table custom-table table table-striped table-hover .table-responsive mt-2'>
-                <thead>
-                <tr className = "table-primary border-dark">
+                <table className='table table-bordered'>
+                  <thead>
+                    <tr>
                       <th>No.</th>
                       <th>Name</th>
                       <th>Email</th>
@@ -201,9 +87,9 @@ const ViewAccount = () => {
                     const imageUrl = userItem.imageUrl;
                     return (
                       <tr key={userItem.id}>
-                        <td  className = "course-info">{id + 1}</td>
-                        <td className = "course-info">{userItem.firstName + " " + userItem.middleName + " " + userItem.lastName}</td>
-                        <td className = "course-info">{userItem.email}</td>
+                        <td>{id + 1}</td>
+                        <td>{userItem.firstName + " " + userItem.middleName + " " + userItem.lastName}</td>
+                        <td>{userItem.email}</td>
                         <td>
                         {imageUrl ? (
                           <div style={{ width: '80px', height: '80px', overflow: 'hidden', borderRadius: '50%' }}>
@@ -213,7 +99,7 @@ const ViewAccount = () => {
                           "No Image Available"
                         )}
                       </td>
-                        <td className = "course-info">{userItem.role}</td>
+                        <td>{userItem.role}</td>
                         <td>
                           <DeleteAccount
                             key={userItem.user_id} // Assign a unique key here
@@ -228,15 +114,10 @@ const ViewAccount = () => {
 
                     </tbody>
                 </table>
-                </div>
               </div>
             </div>
           </div>
         </section>
-        </div>
-            </div>
-          </div>
-          </div>
       </div>
   )
 }
